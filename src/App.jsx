eca989162325d7cd06424fb8014dd575c33c2e26
btabur <webtasarim.0323@gmail.com>
2023-09-29 22:10:38 +0300

    düzenleme modu eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Loading from "./components/Loading";
 
 import "./App.css";
 import ListItem from "./components/ListItem";
+import { v4 as generateId } from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState(null);
@@ -19,12 +20,46 @@ function App() {
     axios.get("http://localhost:3030/todos")
     .then((res) => setTodos(res.data));
   }, []);
+  //! handlesubmit
+  const handleSubmit = (e)=> {
+    e.preventDefault()
+    //console.log(e.target[0].value)
+
+    if(e.target[0].value.trim() === "") {
+      alert("Bir görev ekleyin")
+      e.target[0].value = ""
+      return;
+    }
+
+    const newTodo = {
+    //  id:new Date().getTime(),
+      id:generateId(),
+      title:e.target[0].value,
+      date:new Date().toLocaleDateString(),
+      isDone:false
+    }
+
+    //api ye gönderir
+    axios.post("/todos",newTodo)
+    .then(()=> setTodos([...todos,newTodo]));  // işlem başarılı ise diziye ekleyerek ekranda gösterir
+
+    // input alanını temizler
+    e.target[0].value = ""
+
+    //fetch post
+
+    // fetch('http://localhost:3030/todos', {
+    //   method:'POST',
+    //   body:JSON.stringify(newTodo)
+    // });
+
+  }
 
   return (
     <div className="container my-5">
       <h2>Yapılacaklar</h2>
 
-      <form className="d-flex justify-content-center gap-3 my-4">
+      <form onSubmit={handleSubmit} className="d-flex justify-content-center gap-3 my-4">
         <input className="form-control shadow" />
         <button className="btn btn-primary shadow"> Ekle</button>
       </form>
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,9 +1,14 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
+import {BsFillCheckCircleFill} from 'react-icons/bs';
+import {AiFillCloseCircle} from 'react-icons/ai';
+import {BiSolidEdit} from 'react-icons/bi';
+import {AiTwotoneDelete} from 'react-icons/ai';
 
 axios.defaults.baseURL = 'http://localhost:3030';
 
 function ListItem({todo,allTodos,setTodos}) {
+  const [isEditMode,setIsEditMode] = useState(false)
 
     const handleDelete =()=> {
 
@@ -20,22 +25,75 @@ function ListItem({todo,allTodos,setTodos}) {
           setTodos(filtered)
         })
         .catch(err=> alert('Veri silerken hata oluştu'))
+  }
+  //! handleChange
+    const handleChange =() => {
+      const updated = {...todo,isDone:!todo.isDone}
+
+      // apı yi günceller
+      axios.put(`/todos/${updated.id}`,updated)
+      .then(()=> {      //state i  güncelle
+        const uptatedTodos = allTodos.map((item) => item.id===updated.id ? updated : item);
+        setTodos(uptatedTodos)
+      })
+
+    
+
     }
 
+  //!handleEdit
+    const handleEdit =(event) => {
+      event.preventDefault();
+
+    const newTitle=  event.target[0].value;
+    const updatedTodo = {...todo,title:newTitle};
+
+    // api yi güncelleme
+    axios.put(`/todos/${updatedTodo.id}`,updatedTodo)
+    .then (()=> {
+     const updatedTodos= allTodos.map(item => item.id !== updatedTodo.id ? item : updatedTodo)
+
+     setTodos(updatedTodos)
+
+     setIsEditMode(false)
+    })
+   
+
+    }
 
   return (
     <li className='list-group-item d-flex justify-content-between align-items-center'>
         <div className='d-flex gap-2'>
-            <input className='form-check-input' type="checkbox"/>
+            <input checked={todo.isDone} onChange={handleChange} className='form-check-input' type="checkbox"/>
             <span>
             {todo.isDone ? "Tamamlandı":"Devam ediyor"}
             </span>
         </div>
-        <span>{todo.title}</span>
+         {
+          isEditMode ? 
+              <form onSubmit={handleEdit} className='d-flex gap-3'>
+                <input defaultValue={todo.title} type="text" className='form-control' />
+                <button type='submit'><BsFillCheckCircleFill className='fs-3 text-primary'/></button>  
+                <button type='button' onClick={()=>setIsEditMode(false)}>  
+                  <AiFillCloseCircle className='fs-3 text-danger'/>
+                  </button>  
+              </form> 
+          :
+               <span>{todo.title}</span>
+         }
+      {!isEditMode ? 
         <div className='btn-group'>
-            <button className='btn btn-success'>Düzenle</button>
-            <button className='btn btn-danger' onClick={handleDelete}>Sil</button>
+            <button className='btn btn-success' onClick={()=> setIsEditMode(true)} disabled={isEditMode} >
+              <BiSolidEdit className='fs-4'/>
+            </button>
+            <button className='btn btn-danger' onClick={handleDelete}>
+              <AiTwotoneDelete className='fs-4'/>
+            </button>
         </div>
+      :
+      <span></span>
+      }
+        
     </li>
   )
 }
