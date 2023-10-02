fce4b0ce70005db6cdbba49c42a4e06999d8c5eb
btabur <webtasarim.0323@gmail.com>
2023-10-02 18:13:03 +0300

    added pagination

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,21 @@ import Loading from "./components/Loading";
 import "./App.css";
 import ListItem from "./components/ListItem";
 import { v4 as generateId } from 'uuid';
+import {BsArrowLeftCircleFill,BsArrowRightCircleFill} from 'react-icons/bs'
 
 function App() {
   const [todos, setTodos] = useState(null);
+  const [page,setPage] =useState(1);
+  const [totalPage, setTotalPage] = useState();
+  const [totalCount,setTotalCount] =useState();
+
+  const options = {
+   params: {
+      _limit:5,
+      _page:page
+    },
+    timeout: 5000 // 5sn sonra hala api den veriler gelmediğinde hata fırlatır
+  }
 
   useEffect(() => {
     // fetch get çözümü
@@ -17,9 +29,22 @@ function App() {
     // .then(data=> console.log(data))
 
     // axios
-    axios.get("http://localhost:3030/todos")
-    .then((res) => setTodos(res.data));
-  }, []);
+    axios.get("http://localhost:3030/todos",options)
+    .then((res) => {
+      //apideki toplam eleman sayısına ulaşma
+    const itemCount=  Number(res.headers['x-total-count'])
+    //toplam sayfa sayısını bulma
+    const max = Math.ceil(itemCount/options.params._limit);
+      setTotalPage(max)
+      setTotalCount(itemCount);
+
+      setTodos(res.data)
+    }).catch((err)=>{
+      if(err.code === 'ECONNABORTED') {
+        alert('Üsgünüz bağlantınız zaman aşımına uğradı tekrar deneyiniz')
+      }
+    });
+  }, [page]); //page değeri her değiştiğinde api ye istek atar
   //! handlesubmit
   const handleSubmit = (e)=> {
     e.preventDefault()
@@ -41,7 +66,22 @@ function App() {
 
     //api ye gönderir
     axios.post("/todos",newTodo)
-    .then(()=> setTodos([...todos,newTodo]));  // işlem başarılı ise diziye ekleyerek ekranda gösterir
+    .then(()=> {
+      if(todos.length === options.params._limit) {
+        //son sayfada yer yoksa bir fazlasına 
+        //varsa son sayfaya gönderir
+        setPage(
+          totalCount % options.params._limit === 0 
+          ? totalPage + 1
+          : totalPage
+        )
+      }else {
+        setTodos([...todos,newTodo])
+      }
+ 
+    
+   
+  });  // işlem başarılı ise diziye ekleyerek ekranda gösterir
 
     // input alanını temizler
     e.target[0].value = ""
@@ -73,6 +113,21 @@ function App() {
         setTodos={setTodos}/>
         ))}
       </ul>
+
+      {/* paginaiton */}
+      <div className="d-flex justify-content-center align-items-center mt-5 gap-5">
+        <button 
+        disabled= {page ===1}
+        onClick={()=>setPage(page-1)} >
+          <BsArrowLeftCircleFill className="fs-3 text-warning"/>
+        </button>
+        <p className="fs-3 fw-bold rounded border py-1 px-3">{page}</p>
+        <button
+        disabled = {page === totalPage}
+        onClick={()=> setPage(page+1)}>
+          <BsArrowRightCircleFill className="fs-3 text-primary"/>
+        </button>
+      </div>
     </div>
   );
 }
